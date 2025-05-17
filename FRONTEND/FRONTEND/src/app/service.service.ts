5d8ff404fec4bfd5f88efb20f90e6eeabc3c3321
András Bátori <batori.andras@gmail.com>
2025-05-17 18:32:48 +0200

    service crud implemented

--- FRONTEND/FRONTEND/src/app/service.service.ts
+++ FRONTEND/FRONTEND/src/app/service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Service } from './service';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,49 @@ import { HttpClient } from '@angular/common/http';
 export class ServiceService {
   services: Service[] = []
   localdbname: string = "local_services"
-  apiBaseUrl: string = "https://localhost:7183"
+  apiBaseUrl: string = "https://localhost:7183/api/Services"
   constructor(private http: HttpClient) {
     this.loadServices()
-   }
+  }
 
-  loadServices(): void{
-    this.http.get<Service[]>(this.apiBaseUrl+"/api/Services").subscribe(data=>{
-      this.services=data
+  loadServices(): void {
+    this.http.get<Service[]>(this.apiBaseUrl).subscribe(data => {
+      this.services = data
     })
+  }
+
+  create(service: Service): void {
+    this.http.post(this.apiBaseUrl, service).subscribe({
+      next: (response) => {
+        console.log(response)
+        this.services.push(service)
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+    update(service: Service): void {
+    this.http.put(this.apiBaseUrl+'/'+service.id, service).subscribe({
+      next: (response) => {
+        console.log(response)
+        let index = this.services.findIndex(x => x.id === service.id)
+        this.services[index] = service
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+
+  deleteService(service: Service): void{
+    this.http.delete(this.apiBaseUrl+'/'+service.id).subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: err => {
+        console.error(err)
+      }
+    });
   }
 }
--- FRONTEND/FRONTEND/src/app/services-list/services-list.component.ts
+++ FRONTEND/FRONTEND/src/app/services-list/services-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceService } from '../service.service';
+import { Service } from '../service';
 
 @Component({
   selector: 'app-services-list',
@@ -7,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrl: './services-list.component.sass'
 })
 export class ServicesListComponent {
+  constructor(private router: Router, public servService: ServiceService){}
 
+    remove(service: Service): void {
+    this.servService.deleteService(service.id).subscribe({
+      next: () => {
+        console.log('Szolgáltatás törölve');
+        // Frissítsd a listát vagy állapotot
+      },
+      error: err => {
+        console.error('Hiba a törléskor:', err);
+      }
+    });
+  }
 }
