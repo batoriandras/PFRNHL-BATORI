6cc1450f1de6fe903ac6131958fc71773fff0ee2
András Bátori <batori.andras@gmail.com>
2025-05-17 16:46:08 +0200

    routing implemented with existing components

--- FRONTEND/FRONTEND/src/app/app-routing.module.ts
+++ FRONTEND/FRONTEND/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServicesListComponent } from './services-list/services-list.component';
+import { OrdersListComponent } from './orders-list/orders-list.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"services",pathMatch:"full"},
+  {path:"services", component: ServicesListComponent},
+  {path:"employees", component: EmployeesListComponent},
+  {path:"orders", component: OrdersListComponent},
+  {path:"**",redirectTo:"services",pathMatch:"full"}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
