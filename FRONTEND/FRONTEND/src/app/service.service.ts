9cb88967ee53ba0337c4f539a677bc05de3596dc
András Bátori <batori.andras@gmail.com>
2025-05-17 16:42:59 +0200

    services service loadService added

--- FRONTEND/FRONTEND/src/app/service.service.ts
+++ FRONTEND/FRONTEND/src/app/service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Service } from './service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
+  services: Service[] = []
+  localdbname: string = "local_services"
+  apiBaseUrl: string = "https://localhost:7183"
+  constructor(private http: HttpClient) {
+    this.loadServices()
+   }
 
-  constructor() { }
+  loadServices(): void{
+    this.http.get<Service[]>(this.apiBaseUrl+"/api/Services").subscribe(data=>{
+      this.services=data
+    })
+  }
 }
