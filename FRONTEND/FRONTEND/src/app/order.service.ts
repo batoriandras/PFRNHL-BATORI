2cd02a96764030a43fbdaaf7848705d155a88597
András Bátori <batori.andras@gmail.com>
2025-05-18 18:57:54 +0200

    order crud implemented

--- FRONTEND/FRONTEND/src/app/order.service.ts
+++ FRONTEND/FRONTEND/src/app/order.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Order } from './order';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
+    orders: Order[] = []
+  localdbname: string = "local_orders"
+  apiBaseUrl: string = "https://localhost:7183/api/Order"
+  constructor(private http: HttpClient) {
+    this.loadServices()
+  }
 
-  constructor() { }
+  loadServices(): void {
+    this.http.get<Order[]>(this.apiBaseUrl).subscribe(data => {
+      this.orders = data
+    })
+  }
+
+  create(order: Order): void {
+    this.http.post(this.apiBaseUrl, order).subscribe({
+      next: (response) => {
+        console.log(response)
+        this.orders.push(order)
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+  update(order: Order): void {
+    this.http.put(this.apiBaseUrl + '/' + order.id, order).subscribe({
+      next: (response) => {
+        console.log(response)
+        let index = this.orders.findIndex(x => x.id === order.id)
+        this.orders[index] = order
+      },
+      error: (error) => {
+        console.log(error)
+      }
+    })
+  }
+
+  deleteService(order: Order): void {
+    this.http.delete(this.apiBaseUrl + '/' + order.id).subscribe({
+      next: (response) => {
+        console.log(response)
+        this.orders = this.orders.filter(x => x.id !== order.id)
+      },
+      error: err => {
+        console.error(err)
+      }
+    });
+  }
 }
