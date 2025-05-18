585f6aad3d7fa8b8e09096fd1e00d39672e304fd
András Bátori <batori.andras@gmail.com>
2025-05-18 21:53:32 +0200

    service create form changed

--- FRONTEND/FRONTEND/src/app/service-create/service-create.component.ts
+++ FRONTEND/FRONTEND/src/app/service-create/service-create.component.ts
@@ -13,26 +13,15 @@ import { Router } from '@angular/router';
 })
 export class ServiceCreateComponent {
   service: Service = new Service()
-  serviceForm: FormGroup;
 
   constructor(
-    private http: HttpClient,
     public serService: ServiceService,
     private fb: FormBuilder,
     private router: Router
-    ) {
-    this.serviceForm = this.fb.group({
-      name: ['', [Validators.required, Validators.maxLength(100)]],
-      description: ['', [Validators.maxLength(500)]],
-    });
-  }
+  ) { }
 
   create(): void {
-    if (this.serviceForm.valid) {
-      this.service = this.serviceForm.value
-      this.serService.create(this.service)
-      this.router.navigate(["employees"])
-    }
-    else {alert("hiba")}
+    this.serService.create(this.service)
+    this.router.navigate(["services"])
   }
 }
